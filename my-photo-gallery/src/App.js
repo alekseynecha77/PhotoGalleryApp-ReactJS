8f7e9cbdd01eef39fb15001e9f7d240e47dc1abf
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-11-27 22:08:08 -0500

    Update App.js

--- my-photo-gallery/src/App.js
+++ my-photo-gallery/src/App.js
@@ -25,7 +25,7 @@ export default class App extends Component {
   }
 
   render() { 
-    console.log(this.state.images);
+    console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
